6db5a24c6f2900f33fbcc686c909dcea9a985eca
grupo9codoacodo <119896848+grupo9codoacodo@users.noreply.github.com>
2022-12-07 20:41:52 -0300

    Update request.js

--- src/request.js
+++ src/request.js
@@ -1,9 +1,9 @@
 const API_KEY = "800129ae9583bea7c0b399f1f6078c6d";
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=es-ES`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=es-ES`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with-genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with-genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with-genres=27`,
